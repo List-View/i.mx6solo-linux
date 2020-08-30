e7edff19bbc890c7642cc4cde305b1b77ae9aa58
List-View <13507772589@163.com>
2020-08-30 09:25:33 +0000

    add make.sh

---
+++ make.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+do_env(){
+	export PATH=../u-boot-imx/tools:$PATH
+	export CROSS_COMPILE=/root/data/work/riot-env/fsl-linaro-toolchain/bin/arm-fsl-linux-gnueabi-
+	export ARCH=arm
+}
+
+do_make(){
+	make -j4
+	cp arch/arm/boot/uImage ~/data/
+}
+
+case $1 in
+	(make)
+		do_make
+		;;
+	(env)
+		do_env
+		;;
+	(*)
+		echo "Command no found"
+		;;
+esac
